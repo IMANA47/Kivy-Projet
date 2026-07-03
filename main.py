042ed22ab51ab63e8d8cd3b636c2bacb3cd302de
Francois Nsengimana <francknsengimana@gmail.com>
2026-07-03 09:39:07 +0100

    Add initial Kivy layout and implement autoreload functionality

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy_reloader.app import App
+from kivy.lang import Builder
+
 
 class MainWidget(Widget):
     pass
 class LeLabApp(App):
-    pass
+        # ÉTAPE CLÉ : On dit au reloader quels fichiers surveiller
+    autoreload_files = ["LeLab.kv"] 
 
-LeLabApp().run()
+    def build(self):
+        # On force le nettoyage du cache de Kivy avant de recharger
+        Builder.unload_file("LeLab.kv")
+        return Builder.load_file("LeLab.kv")
+if __name__ == "__main__":
+    LeLabApp().run()
